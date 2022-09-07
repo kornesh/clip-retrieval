7c75104bdd426b2c628f69c7858b39a0324b9736
Kornesh Kanan <kornesh.kanan@gmail.com>
2022-09-07 09:53:09 +0800

    debug

--- clip_retrieval/clip_inference/reader.py
+++ clip_retrieval/clip_inference/reader.py
@@ -185,7 +185,7 @@ def dataset_to_dataloader(dataset, batch_size, num_prepro_workers, input_format)
         shuffle=False,
         num_workers=num_prepro_workers,
         pin_memory=True,
-        prefetch_factor=4,
+        prefetch_factor=8,
         persistent_workers=True,
         collate_fn=collate_fn if input_format == "files" else None,
     )
